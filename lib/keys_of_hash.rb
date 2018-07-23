c1e8fdbc70a12534ad9084941bafba3014ae51be
Felix Chan <felixchan29@gmail.com>
2018-07-23 03:56:19 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,6 +2,10 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     # code goes here
+<<<<<<< HEAD
+=======
+    #binding.pry
+>>>>>>> 38a8c7cd92f6e888937ef9248d43e0eb6f5ef119
     new_array = []
     map do |key, value| 
       if arguments.include?(value)
